b7181584eec49d8cd721bf7749e48c74fffa09c4
ColinCop <colincop@ucm.es>
2016-10-06 23:46:17 +0200

    viernes07

---
+++ god.js
@@ -0,0 +1,37 @@
+﻿
+var enemy = {
+  _currentDirection: 'right',
+  _position: { x: 10, y: 10 },
+  _score: 40,
+
+  moveLeft: function () { console.log('Going left!'); },
+  moveRight: function () { console.log('Going right!'); },
+  advance: function () { console.log('Marching forward!'); },
+  shoot: function () { console.log('PICHIUM!'); } // (es un láser)
+};
+
+
+
+var nave= {
+  _currentDirection: 'left',
+  _position: { x: 10, y: 0 },
+  
+
+  moveLeft: function () { console.log('Going left!'); },
+  moveRight: function () { console.log('Going right!'); },
+  advance: function () { console.log('Marching forward!'); },
+  shoot:function () { console.log('DESTROY!'); }
+}; 
+function disparo(nave,pos){
+var bala = { };
+bala._pos=pos;
+
+if(nave==='enemy'){
+obj._direccion = 'abajo';
+}
+else{
+obj._direccion = 'arriba';
+};
+
+}
+
